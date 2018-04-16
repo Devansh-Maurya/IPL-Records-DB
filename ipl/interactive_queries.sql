294d48492facdc4b43a01068adcc4c9b0dbf296b
Ali Akbar <aliakbar09a@gmail.com>
2018-04-16 00:41:43 -0400

    3 sql entries added from updateddetails.php

--- ipl/interactive_queries.sql
+++ ipl/interactive_queries.sql
@@ -11,3 +11,21 @@ INSERT INTO player_bowling
         VALUES ('$first_name', '$last_name', '$bowling_innings',
         '$bowling_ball', '$bowling_runs_given', '$bowling_wicket', '$bowling_fifer',
         '$bowling_tenfer');   
+
+UPDATE player_personal SET  
+        date_of_birth = '$date_of_birth', birth_city = '$birth_city', position = '$position', 
+        team = '$team', match_played = '$match_played', birth_state = '$birth_state'
+        WHERE first_name = '$first_name' AND last_name = '$last_name';
+
+UPDATE player_batting SET 
+        innings = '$batting_innings', not_out = '$batting_not_out', 
+        runs_scored = '$batting_runs_scored', highscore = '$batting_highscore', ball_faced = '$batting_ball_faced',
+        hundred = '$batting_hundred', double_hundred = '$batting_double_hundred', fifty = '$batting_fifty',
+        four = '$batting_four', six = '$batting_six'
+        WHERE first_name = '$first_name' AND last_name = '$last_name';
+
+UPDATE player_bowling SET 
+        innings = '$bowling_innings', ball = '$bowling_ball', wicket = '$bowling_wicket', 
+        runs_given = '$bowling_runs_given', fifer = '$bowling_fifer', tenfer = '$bowling_tenfer'
+        WHERE first_name = '$first_name' AND last_name = '$last_name';
+
